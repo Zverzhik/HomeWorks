31330ec79eff8711a10ada394838c5938823ca84
Zverzhik <zverdasha1245@mail.ru>
2024-07-25 11:30:44 +0300

    Initial commit

---
+++ src/main/java/ru/otus/project/ucheba/Homework10/PhoneBook.java
@@ -0,0 +1,51 @@
+package ru.otus.project.ucheba.Homework10;
+
+import java.util.*;
+
+public class PhoneBook {
+    private final Map<String, Set<String>> phoneBook = new HashMap<>();
+
+    public void add(String fullname, String phoneNumber) {
+        Set<String> phones = phoneBook.get(fullname);
+        if (phones != null) {
+            phones.add(phoneNumber);
+        } else {
+            phones = new HashSet<>();
+            phones.add(phoneNumber);
+            phoneBook.put(fullname, phones);
+        }
+    }
+    public void find(String fullname) {
+        for (Map.Entry<String,Set<String>> entry : phoneBook.entrySet()) {
+            if (fullname.equals(entry.getKey())) {
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+    public boolean containsPhoneNumber(String phoneNumber) {
+        for (Map.Entry<String, Set<String>> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return true;
+            }
+        }
+     return false;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        PhoneBook phoneBook1 = (PhoneBook) object;
+        return Objects.equals(phoneBook, phoneBook1.phoneBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(phoneBook);
+    }
+
+    @Override
+    public String toString() {
+        return "Телефонная книжка: " + phoneBook;
+    }
+}
